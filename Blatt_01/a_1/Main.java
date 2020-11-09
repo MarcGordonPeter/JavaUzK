79f7db6843ae5f2ca78df82042cf0bc36edc5115
MGP <marcpeter.dus@web.de>
2020-11-09 13:55:47 +0100

    Initial Commit

--- Blatt_01/a_1/Main.java
+++ Blatt_01/a_1/Main.java
@@ -1,9 +1,1 @@
-package a_1;
-
-public class Main {
-	public static void main(String[]args) {
-		int i = 1;
-		int j = 2;
-		System.out.println(i+j);
-	}
-}
+package a_1;public class Main{public static void main(String[]args){int i=1;int j=2;System.out.println(i+j);}}
