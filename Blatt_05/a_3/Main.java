fc0c9324e7266d167df80a392c1bf4522dcf613c
MGP <marcpeter.dus@web.de>
2020-12-07 11:53:26 +0100

    Aufgabe 3 geloest

---
+++ Blatt_05/a_3/Main.java
@@ -0,0 +1,13 @@
+package a_3;
+
+public class Main {
+
+	public static void main(String[]args) {
+		int[][] a= {{1,2,3}, {1,2,3,4}, {1,7}};
+		for(int i=0; i<a.length;i++ ) {
+			for(int j=0;j<a[i].length;j++) {
+				System.out.print(a[i][j]);
+			}
+		}
+	}
+}
