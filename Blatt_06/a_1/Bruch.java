fad3a38d9abf103fffe9d101ed6b2855a27fdf57
MGP <marcpeter.dus@web.de>
2020-12-14 10:37:35 +0100

    Blatt6 Aufgabe 1

---
+++ Blatt_06/a_1/Bruch.java
@@ -0,0 +1,64 @@
+package a_1;
+
+public class Bruch {
+	int zaehler;
+	int nenner;
+	
+	Bruch() {
+		zaehler = 0;
+		nenner = 1;
+	}
+	
+	Bruch(int z, int n) {
+		zaehler = z;
+		nenner= n;
+		standardisiere();
+	}
+	
+	void print() {
+		System.out.println(zaehler + "/" + nenner);
+	}
+	
+	boolean gueltig() {
+		if (nenner == 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	void standardisiere() {
+		if (zaehler < 0 && nenner < 0) {
+			zaehler = -zaehler;
+			nenner = -nenner;
+		} else if (nenner < 0) {
+			nenner = -nenner;
+		}
+	}
+	
+	void multipliziere(int f) {
+		zaehler *= f;
+	}
+	
+	void multipliziere(Bruch f) {
+		zaehler *= f.zaehler;
+		nenner *= f.nenner;
+	}
+	
+	boolean groesser_als(int f) {
+		double b = zaehler / nenner;
+		if (b > (double)f) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	boolean groesser_als(Bruch f) {
+		if(((double)zaehler/(double)nenner) > ((double)f.zaehler/(double)f.nenner)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
---
+++ Blatt_06/a_1/Main.java
@@ -0,0 +1,32 @@
+package a_1;
+
+public class Main {
+
+	public static void main(String[]args) {
+		Bruch a = new Bruch(4,3);
+		Bruch b = new Bruch(1,5);
+		Bruch c = new Bruch(1,0);
+		int mult = 5;
+		
+		System.out.println("a ist gültig: " + a.gueltig());
+		System.out.println("b ist gültig: " + b.gueltig());
+		System.out.println("c ist gültig: " + c.gueltig());
+		
+		System.out.println("a ist groesser als b: " + a.groesser_als(b));
+		System.out.println("b ist groesser als a: " + b.groesser_als(a));
+		
+		
+		System.out.println("a*5:");
+		a.multipliziere(5);
+		a.print();
+		
+		System.out.println("b*a:");
+		b.multipliziere(a);
+		b.print();
+		
+		System.out.println("a ist groesser als 5: " + a.groesser_als(mult));
+		System.out.println("b ist groesser als 5: " + b.groesser_als(mult));
+		
+	}
+	
+}
