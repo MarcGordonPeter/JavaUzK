74b22849a6aa4e5056be253fab5d8f39e4fc904f
MGP <marcpeter.dus@web.de>
2020-12-31 19:22:19 +0100

    Aufgabe 3 fertiggestellt

--- Blatt_07/a_3/Main.java
+++ Blatt_07/a_3/Main.java
@@ -1,5 +1,30 @@
 package a_3;
 
 public class Main {
-
+	
+	public static void main(String[]args) {
+		int n = 0;
+		System.out.println(rekursivFakultaet(n));
+		System.out.println(iterativFakultaet(n));
+	}
+	
+	static int iterativFakultaet(int n) {
+		if(n==0) {
+			return 1;
+		} else {
+			int res = 1;
+			for(int i=1;i<n+1;i++) {
+				res *= i;
+			}
+			return res;
+		}
+	}
+	
+	static int rekursivFakultaet(int n) {
+		if (n == 1 || n==0){
+	        return 1;
+	    }
+	    int res = rekursivFakultaet(n - 1);
+	    return res * n;
+	}
 }
