5f5389b3e45b245d53a666e9fcc0fb8513e405d9
MGP <marcpeter.dus@web.de>
2021-01-07 21:21:40 +0100

    fibonacci rekursiv fertiggestellt

--- Blatt_07/a_4/Main.java
+++ Blatt_07/a_4/Main.java
@@ -3,8 +3,16 @@ package a_4;
 public class Main {
 	
 	public static void main(String[]args) {
-		for(int i=0;i<20;i++) {
-			System.out.println(fibonacciIterativ(i));
+		int n = 10; //max: 46
+		
+		System.out.println("Iterativ bis " + n + ":");
+		for(int i=0;i<=n;i++) {
+			System.out.println("a" + i + ": " + fibonacciIterativ(i));
+		}
+		
+		System.out.println("Rekursiv bis " + n + ":");
+		for(int i=0;i<=n;i++) {
+			System.out.println("a" + i + ": " + fibonacciRekursiv(1,1,i));
 		}
 	}
 	
@@ -13,8 +21,7 @@ public class Main {
 			return 1;
 		} else {
 			n++;
-			int a1=1 ,an= 1;
-			int a2;
+			int a1 = 1, a2 ,an = 1;
 			while(n>2) {
 				a2 = a1;
 				a1 = an;
@@ -22,6 +29,19 @@ public class Main {
 				n--;
 			}
 			return an;
+		}
+	}
+	
+	static int fibonacciRekursiv(int a1, int a2, int n) {
+		if(n==0 || n==1) {
+			return 1;
+		} else {
+			int an = a1 + a2;
+			n--;
+			if (n < 2) {
+				return an;
+			}
+			return fibonacciRekursiv(an,a1,n);
 		}
 	}
 }
