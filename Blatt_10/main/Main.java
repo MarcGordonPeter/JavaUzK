b6d5b5dfe8ab6930ae9efee7ac05de793edcb078
MGP <marcpeter.dus@web.de>
2021-01-25 13:11:53 +0100

    teil 0 und 1 fertiggestellt

--- Blatt_10/main/Main.java
+++ Blatt_10/main/Main.java
@@ -1,8 +1,21 @@
 package main;
 
+import werkzeuge.HalloWelt;
+import werkzeuge.math.*;
+
 public class Main {
 
 	public static void main(String[]args) {
+		werkzeuge.HalloWelt.hallo_welt();
+		werkzeuge.HalloWelt.hallo();
 		
+		HalloWelt.hallo_welt();
+		HalloWelt.hallo();
+		
+		System.out.println(werkzeuge.math.Fibonacci.berechne(12));
+		System.out.println(werkzeuge.math.Fakultaet.berechne(6));
+		
+		System.out.println(Fibonacci.berechne(12));
+		System.out.println(Fakultaet.berechne(6));
 	}
 }
