fbd29a2b0ba38f66d14e366fd01a3aa9fd9bd152
MGP <marcpeter.dus@web.de>
2021-02-02 14:25:00 +0100

    Aufgabe 1 verbessert

--- Blatt_10_1/a_1/Einfach_verkettete_Liste.java
+++ Blatt_10_1/a_1/Einfach_verkettete_Liste.java
@@ -6,14 +6,22 @@ public class Einfach_verkettete_Liste {
 	public Einfach_verkettete_Liste(int w) {
 		kopf = new Knoten(w, null);
 	}
-	
+	/*
 	public Einfach_verkettete_Liste(int[] arr) {
 		kopf = new Knoten(arr[arr.length-1], null);
 		for(int i=arr.length - 2; i > -1; i--) {
 			kopf = new Knoten(arr[i], kopf);
 		}
 	}
-
+	*/
+	public Einfach_verkettete_Liste(int[] arr) {
+		kopf = new Knoten(arr[0], null);
+		Knoten k = kopf;
+		for(int i=1; i < arr.length; i++) {
+			k = fuege_ein_nach(k, arr[i]);
+		}
+	}
+	
 	public void print() {
 		for( Knoten k = kopf; k != null; k = k.next) {
 			System.out.println(k.wert + " ");
