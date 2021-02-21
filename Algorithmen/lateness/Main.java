fcbb4f808b0dddc41ccd755f84ca2876ce2f82e3
MGP <marcpeter.dus@web.de>
2021-02-21 13:45:07 +0100

    bessere Ausgabe bei Lateness

--- Algorithmen/lateness/Main.java
+++ Algorithmen/lateness/Main.java
@@ -6,13 +6,16 @@ public class Main {
 	
 	public static void main(String[]args) {
 		Anfrage[] A_array = {
-				new Anfrage(4,9),new Anfrage(3,6),new Anfrage(4,8),new Anfrage(1,3),new Anfrage(4,6)
+				new Anfrage(3,11),new Anfrage(3,13),new Anfrage(1,14),new Anfrage(1,9),new Anfrage(2,18)
 		};
 		indexList A = new indexList(A_array);
 
-		Termin[] t = minimizeLateness(A, 0);
+		Termin[] t = minimizeLateness(A, 8);
+		System.out.println("Start:\tFaellig:\tEnde:\tlate:");
 		for(int i=0 ; i< t.length;i++) {
-			System.out.println("" + t[i].a.d +" "+ t[i].a.f);
+			int ende = t[i].s+t[i].a.d;
+			int late = (ende < t[i].a.f) ? 0 : ende - t[i].a.f;
+			System.out.println("" + t[i].s +"\t "+ t[i].a.f + "\t\t" + ende + "\t" + late );
 		}
 	}
 	
